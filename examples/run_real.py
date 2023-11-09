640c48f3fcac0d806ee6af0fdd022b6ff9b2823f
rcabdia <rcabdia@gmail.com>
2023-11-09 14:18:39 +0100

    working on real example

--- examples/run_real.py
+++ examples/run_real.py
@@ -1,9 +1,10 @@
-from surfquakecore.utils import obspy_utils
 
+#path_to_configfiles =
 inventory_path = "/Users/roberto/Documents/SurfQuakeCore/tests/test_resources/mti/inventories/inv_surfquakecore.xml"
 picks_path = '/Volumes/LaCie/test_surfquake_core/test_picking'
 working_directory = '/Volumes/LaCie/test_surfquake_core/test_real'
 
-#inventory_path, path_to_configfiles, working_directory, output_directory
+#RealCore(inventory_path, path_to_configfiles, working_directory, output_directory)
+
 
 #obspy_utils.ObspyUtil.realStation(inventory, working_directory)
