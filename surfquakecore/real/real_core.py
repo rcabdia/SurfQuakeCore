0456495e1d487c7ce4413944cf5287bc13351608
rcabdia <rcabdia@gmail.com>
2023-11-10 10:30:27 +0100

    Updated ttdb in real and corresponding running example

--- surfquakecore/real/real_core.py
+++ surfquakecore/real/real_core.py
@@ -1,9 +1,10 @@
+import glob
+import os
 from obspy import read_inventory
 from obspy.geodetics import gps2dist_azimuth, kilometers2degrees
 from surfquakecore.real.real_parse import load_real_configuration
 from surfquakecore.real.tt_db.taup_tt import create_tt_db
 from surfquakecore.utils import obspy_utils
-
 
 class RealCore:
     def __init__(self, metadata_file: str, parameters_file: str, working_directory: str,
@@ -21,6 +22,22 @@ class RealCore:
         self.parameters_file = parameters_file
         self.working_directory = working_directory
         self.output_directory = output_directory
+        self._start_folder_tree()
+
+
+    def _start_folder_tree(self):
+        if not os.path.exists(self.working_directory):
+            os.makedirs(self.working_directory)
+        else:
+            files = glob.glob(self.working_directory + "/*")
+            for f in files:
+                os.remove(f)
+        if not os.path.exists(self.output_directory):
+            os.makedirs(self.output_directory)
+        else:
+            files = glob.glob(self.output_directory + "/*")
+            for f in files:
+                os.remove(f)
 
     def _return_inventory(self):
         inv = {}
@@ -44,11 +61,13 @@ class RealCore:
         self.h_range = kilometers2degrees(distance*0.001)
 
     def run_real(self):
-        inventory = self._get_files_from_config(self.metadata_file)
-        obspy_utils.ObspyUtil.realStation(inventory, self.working_directory)
+        inventory = self._return_inventory()
         self._get_files_from_config(self.parameters_file)
+
+        obspy_utils.ObspyUtil.realStation(inventory, self.working_directory)
 
         # create travel times
         self._get_real_grid()
         tt_db = create_tt_db()
-        tt_db.run_tt_db(dist=self.h_range, depth=self.parameters['geographic_frame']['depth'], ddist=0.01, ddep=1)
+        tt_db.run_tt_db(ttime=self.working_directory, dist=self.h_range,
+                        depth=self.parameters['geographic_frame']['depth'], ddist=0.01, ddep=1)
