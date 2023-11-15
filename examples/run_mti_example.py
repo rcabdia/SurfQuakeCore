04d79ce47688ccf237bc5fe50c5356e97e79e612
Roberto Cabieces Díaz <rcabdia@gmail.com>
2023-11-15 21:21:55 +0100

    Included MTI example ready to test binaries

--- examples/run_mti_example.py
+++ examples/run_mti_example.py
@@ -5,25 +5,25 @@ from surfquakecore.utils.obspy_utils import MseedUtil
 import warnings
 warnings.filterwarnings("ignore")
 if __name__ == "__main__":
-
-    inventory_path = "/Users/roberto/Documents/SurfQuakeCore/tests/test_resources/mti/inventories/inv_surfquakecore.xml"
-    data_dir_path = "/Users/roberto/Desktop/surfquakecore_test_data"
-    path_to_project = "/Users/roberto/Documents/SurfQuakeCore/tests/test_resources/mti/project"
-    project_name = 'surfquake_project_test.pkl'
-    path_to_configfiles = '/Users/roberto/Documents/SurfQuakeCore/tests/test_resources/mti/list_earthquakes'
-    working_directory = "/Volumes/LaCie/mti_surfquakecore/working_directory"
-    output_directory = "/Volumes/LaCie/mti_surfquakecore/output_directory"
+    cwd = os.getcwd()
+    inventory_path = os.path.join(cwd, "mti/inventories/inv_surfquakecore.xml")
+    data_dir_path = os.path.join(cwd, "mti/waveforms")
+    path_to_project = os.path.join(cwd, "mti/project")
+    project_name = os.path.join(cwd, 'surfquake_project_test.pkl')
+    path_to_configfiles = os.path.join(cwd, 'mti/list_earthquakes')
+    working_directory = os.path.join(cwd, "mti/working_directory")
+    output_directory = os.path.join(cwd, "mti/output_directory")
 
     project_tobe_saved = os.path.join(path_to_project, project_name)
-    #ms = MseedUtil()
-    #project = ms.search_files(data_dir_path)
-    #print("End of project creation, number of files ", len(project))
+    ms = MseedUtil()
+    project = ms.search_files(data_dir_path)
+    print("End of project creation, number of files ", len(project))
 
     # it is possible to save the project for later use
     #project = ms.save_project(project, project_tobe_saved)
 
     # alternatively one can load the project
-    project = MseedUtil.load_project(file=project_tobe_saved)
+    #project = MseedUtil.load_project(file=project_tobe_saved)
 
     # build the class
     bic = BayesianIsolaCore(project, inventory_path, path_to_configfiles, working_directory, output_directory,
