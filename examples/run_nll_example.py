6b5331da7b3affdd9a3be5f5e9a469172c8f43c0
Thiago C. Junqueira <junqfisica@hotmail.com>
2023-12-02 10:37:03 +0100

    Thiago - Removed unecessary files from git

--- examples/run_nll_example.py
+++ examples/run_nll_example.py
@@ -1,16 +1,16 @@
 import os
 
-from surfquakecore.earthquake_location.run_nll import NllManager, Nllcatalog
+from surfquakecore.earthquake_location.run_nll import Nllcatalog, NllManager
 
 if __name__ == "__main__":
     cwd = os.path.dirname(__file__)
-    ## Basic input: working_directory, invenoty file path and config_file input
+    # Basic input: working_directory, inventory file path and config_file input
     working_directory = os.path.join(cwd, "earthquake_locate")
     inventory_path = os.path.join(working_directory, "inventories", "inv_surfquakecore.xml")
     path_to_configfiles = os.path.join(working_directory, "config/nll_config.ini")
-    #nll_manager = NllManager(path_to_configfiles, inventory_path, working_directory)
-    #nll_manager.vel_to_grid()
-    #nll_manager.grid_to_time()
-    #nll_manager.run_nlloc()
+    nll_manager = NllManager(path_to_configfiles, inventory_path, working_directory)
+    nll_manager.vel_to_grid()
+    nll_manager.grid_to_time()
+    nll_manager.run_nlloc()
     nll_catalog = Nllcatalog(working_directory)
     nll_catalog.run_catalog(working_directory)
