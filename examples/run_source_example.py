6872a53727673dcff0313290828d7e54dd1dab5c
rcabdia <rcabdia@gmail.com>
2024-01-04 14:21:02 +0100

    Upgraded example of source_spec

--- examples/run_source_example.py
+++ examples/run_source_example.py
@@ -1,16 +1,15 @@
 from surfquakecore.magnitudes.run_magnitudes import Automag
 from surfquakecore.magnitudes.source_tools import ReadSource
-from surfquakecore.utils.obspy_utils import MseedUtil
+from surfquakecore.project.surf_project import SurfProject
 import os
 
 if __name__ == "__main__":
 
     cwd = os.path.dirname(__file__)
     ## Project definition ##
-    path_to_project = "source_estimations/data"
-    project_tobe_saved = os.path.join(path_to_project, "surfquake_project_test.pkl")
-    print("project:", project_tobe_saved)
-    ms = MseedUtil()
+    path_to_project = "/Volumes/LaCie/test_surfquake_core/testing_data"
+    project_path_file = os.path.join(path_to_project, "surfquake_project_new.pkl")
+    print("project:", project_path_file)
     #project = ms.search_files(path_to_project)
     #print("End of project creation, number of files ", len(project))
 
@@ -18,8 +17,9 @@ if __name__ == "__main__":
     #project = ms.save_project(project, project_tobe_saved)
 
     # alternatively one can load the project
-    project = MseedUtil.load_project(file=project_tobe_saved)
-    print(project)
+    sp_loaded = SurfProject.load_project(path_to_project_file=project_path_file)
+    print(sp_loaded)
+
     ## Basic input: working_directory, invenoty file path and config_file input
     working_directory = os.path.join(cwd, "source_estimations")
 
@@ -31,8 +31,8 @@ if __name__ == "__main__":
     summary_path = '/Users/roberto/Documents/SurfQuakeCore/examples/source_estimations/source_summary'
 
     # Running stage
-    #mg = Automag(project, locations_directory, inventory_path, path_to_configfiles, output_directory, "regional")
-    #mg.estimate_source_parameters()
+    mg = Automag(sp_loaded.project, locations_directory, inventory_path, path_to_configfiles, output_directory, "regional")
+    mg.estimate_source_parameters()
 
     # Now we can read the output and even write a txt summarizing the results
     rs = ReadSource(output_directory)
