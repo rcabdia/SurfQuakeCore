5f04cfe491e20a1d5426790ea7cef0c9ec70087a
Roberto Cabieces Díaz <rcabdia@gmail.com>
2024-02-16 20:49:05 +0100

    Improve of catalog manage class

--- examples/run_catalog.py
+++ examples/run_catalog.py
@@ -3,9 +3,16 @@ from surfquakecore.utils.manage_catalog import WriteCatalog
 catalog_path = "/Volumes/LaCie/all_andorra/catalog/catalog_obj.pkl"
 output_path = "/Volumes/LaCie/all_andorra/catalog/catalog_surf.txt"
 wc = WriteCatalog(catalog_path)
+print(wc.show_help())
+help(wc.filter_time_catalog)
+help(wc.filter_geographic_catalog)
 catalog_filtered = wc.filter_time_catalog(starttime="30/01/2022, 00:00:00.0", endtime="20/02/2022, 00:00:00.0")
+
 catalog_filtered = wc.filter_geographic_catalog(catalog_filtered, lat_min=42.1, lat_max=43.0, lon_min=0.8, lon_max=1.5,
                                                 depth_min=-10, depth_max=20, mag_min=3.4, mag_max=3.9)
-print(catalog_filtered.__str__(print_all=True))
-catalog_filtered.plot(projection='local')
-#wc.write_catalog_surf(output_path)
+
+wc.write_catalog_surf(catalog=None, output_path=output_path)
+#wc.write_catalog_surf(catalog=catalog_filtered, output_path=output_path)
+
+# Now you can also save the filtered catalog
+#catalog_filtered.plot(projection='local')
--- surfquakecore/utils/manage_catalog.py
+++ surfquakecore/utils/manage_catalog.py
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------
+# Filename: manage_catalog.py
+# Program: surfQuake & ISP
+# Date: February 2024
+# Purpose: Manage Catalog
+# Author: Roberto Cabieces & Thiago C. Junqueira
+#  Email: rcabdia@roa.es
+# --------------------------------------------------------------------
+
 import pickle
 import pandas as pd
-#from obspy import read_events
 import os
 from obspy.core.event import Magnitude, FocalMechanism, MomentTensor, Tensor, Catalog
 from surfquakecore.utils import read_nll_performance
@@ -10,6 +19,22 @@ from typing import Union
 class BuildCatalog:
     def __init__(self, loc_folder, output_path, format="QUAKEML", source_summary_file=None, mti_summary_file=None):
 
+        """
+        BuildCatalog class helps to join information from all surfquake outputs and create a catalog
+
+        Attributes:
+        - loc_folder (str): Path to the folder where the user have the locations files *hyp
+        - output_path (str): Output folder path where catalog object and file will be saved
+        - format (str): https://docs.obspy.org/packages/autogen/obspy.core.event.Catalog.write.html
+        - source_summary_file (str): Path to the output file from source module
+        - mti_summary_file (str): Path to the output file from mti module
+
+        Methods:
+        - __init__(root_path): Initialize a new instance of BuildCatalog.
+        - __merge_info(catalog: Catalog): Merges the information from loc, source and mti
+        - build_catalog(): Starts the process to create the catalog from loc files, then calls __merge_info
+        """
+
         self.loc_folder = loc_folder
         self.source_summary_file = source_summary_file
         self.mti_summary_file = mti_summary_file
@@ -17,12 +42,12 @@ class BuildCatalog:
         self.format = format
 
         if source_summary_file != None:
-            self.df = pd.read_csv(self.source_summary_file, sep = ";", index_col="date_id")
+            self.df = pd.read_csv(self.source_summary_file, sep=";", index_col="date_id")
 
         if mti_summary_file != None:
-            self.df_mti = pd.read_csv(self.mti_summary_file, sep = ";", index_col="date_id")
+            self.df_mti = pd.read_csv(self.mti_summary_file, sep=";", index_col="date_id")
 
-    def list_directory(self):
+    def __list_directory(self):
         obsfiles = []
         for top_dir, sub_dir, files in os.walk(self.loc_folder):
             for file in files:
@@ -30,7 +55,7 @@ class BuildCatalog:
         obsfiles.sort()
         return obsfiles
 
-    def merge_info(self, catalog):
+    def __merge_info(self, catalog):
         for i, ev in enumerate(catalog):
 
             for origin in ev.origins:
@@ -98,14 +123,14 @@ class BuildCatalog:
 
 
     def build_catalog_loc(self):
-        events_files = self.list_directory()
+        events_files = self.__list_directory()
         catalog = Catalog()
         for file_ev in events_files:
             try:
                 catalog += read_nll_performance.read_nlloc_hyp_ISP(file_ev)
             except:
                 pass
-        catalog = self.merge_info(catalog)
+        catalog = self.__merge_info(catalog)
         print(catalog.__str__(print_all=True))
         catalog_file_name = os.path.join(self.output_path, "catalog")
         catalog_file_name_pkl = os.path.join(self.output_path, "catalog_obj.pkl")
@@ -117,20 +142,65 @@ class BuildCatalog:
 
 class WriteCatalog:
     def __init__(self, path_catalog):
+
+        """
+        WriteCatalog class helps to filter the catalog obj write in the most Readable
+
+        Attributes:
+        - path_catalog (str): Path to the pickle file saved when run build_catalog_loc from BuildCatalog class
+
+        Methods:
+        - __init__(root_path): Initialize a new instance of BuildCatalog.
+        - filter_time_catalog(verbose=True, **kwargs): filter the catalog in time span
+        - filter_geographic_catalog(verbose=True, **kwargs): filter the catalog in geographic contrain and magnitude
+        - write_catalog(catalog: Catalog, format, output_path)
+        - write_catalog_surf(catalog: Union[Catalog, None], output_path)
+        """
+
         self.path_catalog = path_catalog
         self.catalog = []
         self.__test_catalog()
 
+    def show_help(self):
+
+        """
+        Display help for the WriteCatalog class.
+
+        Usage:
+        wc = WriteCatalog(catalog_path)
+        help(wc.filter_time_catalog)
+        help(wc.filter_geographic_catalog)
+        catalog_filtered = wc.filter_time_catalog(starttime="30/01/2022, 00:00:00.0", endtime="20/02/2022, 00:00:00.0")
+
+        catalog_filtered = wc.filter_geographic_catalog(catalog_filtered, lat_min=42.1, lat_max=43.0, lon_min=0.8, lon_max=1.5,
+                                                        depth_min=-10, depth_max=20, mag_min=3.4, mag_max=3.9)
+        wc.write_catalog_surf(catalog=catalog_filtered, output_path=output_path)
+        """
+        return self.__doc__
+
     def __test_catalog(self):
-        catalog = []
+
         try:
             self.catalog = pd.read_pickle(self.path_catalog)
             print(self.catalog.__str__(print_all=True))
         except:
             raise ValueError("file is not a valid catalog")
 
-    def filter_time_catalog(self, **kwargs):
-        # Date input format "%d/%m/%Y, %H:%M:%S.%f"
+    def filter_time_catalog(self, verbose=True, **kwargs):
+
+        """
+        Filter the catalog readed in the class instantiation
+        verbose: bool:
+        **kwargs
+        starttime :str: starttime to filter the catalog in format %d/%m/%Y, %H:%M:%S.%f
+        endtime :str: endtime to filter the catalog in format %d/%m/%Y, %H:%M:%S.%f
+        example:
+        wc = WriteCatalog(catalog_path)
+        catalog_filtered = wc.filter_time_catalog(starttime="30/01/2022, 00:00:00.0",
+        endtime="20/02/2022, 00:00:00.0")
+        return :catalog obj:
+        """
+
         date_format = "%d/%m/%Y, %H:%M:%S.%f"
         catalog_filtered = Catalog()
         starttime = kwargs.pop('starttime', [])
@@ -150,10 +220,32 @@ class WriteCatalog:
         if len(catalog_filtered) == 0:
             print("Check if there is events in catalog time span or the format is valid , ", date_format)
 
+        if verbose:
+            print(catalog_filtered.__str__(print_all=True))
 
         return catalog_filtered
 
-    def filter_geographic_catalog(self, catalog: Union[Catalog, None], **kwargs):
+    def filter_geographic_catalog(self, catalog: Union[Catalog, None], verbose= True, **kwargs):
+
+        """
+        Filter the catalog readed in the class instantiation or the catalog provided in bu the user when the method
+        is called
+        verbose: bool:
+        catalog obj (optional), if not found it uses catalog from the catalog attribute
+        **kwargs --> All keys must be used to filter success.
+
+        lat_min:float
+        lat_max:float
+        lon_min:float
+        lon_max:float
+        depth_min:float: km
+        depth_max:float: km
+        mag_min:float
+        mag_max:float
+
+        return :catalog obj:
+        """
+
         if catalog is None:
             # This option proceed to filter the attribute catalog
             catalog = self.catalog.copy()
@@ -187,11 +279,37 @@ class WriteCatalog:
                             if isinstance(magnitude, float) and mag_min <= magnitude <= mag_max:
                                 catalog_filtered += ev
 
+        if verbose:
+            print(catalog_filtered.__str__(print_all=True))
+
         return catalog_filtered
 
-    def write_catalog_surf(self, output_path):
+    def write_catalog(self, catalog: Catalog, format: str, output_path: str):
+
+        catalog_file_name = os.path.join(self.output_path, "catalog")
+        catalog_file_name_pkl = os.path.join(output_path, "catalog_obj.pkl")
+        catalog.write(filename=catalog_file_name, format=format)
+
+        with open(catalog_file_name_pkl, 'wb') as file:
+            pickle.dump(catalog, file)
+
+        pd.to_pickle(catalog, filepath_or_buffer=catalog_file_name_pkl)
+        print("Catalog saved at ", catalog_file_name, " format ", self.format)
+
+    def write_catalog_surf(self, catalog: Union[Catalog, None], output_path):
+
+        """
+        Writes in human language the catalog instantiated with the class
+        verbose: bool:
+        catalog obj (optional), if not found it uses catalog from the catalog attribute
+        """
+
+        if catalog is None:
+            # This option proceed to filter the attribute catalog
+            catalog = self.catalog.copy()
+
         with open(output_path, 'w') as file:
-            for i, ev in enumerate(self.catalog):
+            for i, ev in enumerate(catalog):
                 for origin in ev.origins:
 
                     lat_origin = "{:.4f}".format(origin.latitude)
@@ -200,10 +318,10 @@ class WriteCatalog:
                     min_hor_unc = "{:.4f}".format(origin.origin_uncertainty.min_horizontal_uncertainty * 1E-3)
                     max_hor_unc = "{:.4f}".format(origin.origin_uncertainty.max_horizontal_uncertainty * 1E-3)
                     gap = "{:.4f}".format(origin.origin_uncertainty.azimuth_max_horizontal_uncertainty * 1E-3)
-                    confidence_level = "{:.4f}".format(origin.origin_uncertainty.confidence_level)
+                    confidence_level = "{:.1f}".format(origin.origin_uncertainty.confidence_level)
                     file.write(f"Event {i+1}: Lat {lat_origin} Lon {lon_origin} Depth {depth_origin} km "
-                               f"min_hor_unc {min_hor_unc} km max_hor_unc {max_hor_unc} gap {gap} "
-                               f"conf_lev {confidence_level}\n")
+                               f"min_hor_unc {min_hor_unc} km max_hor_unc {max_hor_unc} km gap {gap} "
+                               f"conf_lev {confidence_level} %\n")
 
                     # magnitudes
                     if len(ev.magnitudes) > 0:
@@ -218,8 +336,6 @@ class WriteCatalog:
 
                     file.write(f"station phase polarity date time time_residual time_weight distance_degrees "
                                    f"distance_km  azimuth takeoff_angle\n")
-
-                    #file.write(f"\n")
 
                     for arrival in origin.arrivals:
                         azimuth = "{:.1f}".format(arrival.azimuth)
@@ -240,15 +356,15 @@ class WriteCatalog:
 
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # path_events_file = "/Volumes/LaCie/surfquake_test/test_nll_final"
     # path_source_file = "/Volumes/LaCie/surfquake_test/catalog_output/sources.txt"
     # output_path = "/Volumes/LaCie/surfquake_test/catalog_output"
     #bc = BuildCatalog(loc_folder=path_events_file, source_summary_file=path_source_file, output_path=output_path,
     #                  format="QUAKEML")
     #bc.build_catalog_loc()
-    catalog_path = "/Volumes/LaCie/all_andorra/catalog/catalog_obj.pkl"
-    output_path = "/Volumes/LaCie/all_andorra/catalog/catalog_surf.txt"
-    wc = WriteCatalog(catalog_path)
-    wc.write_catalog_surf(output_path)
+    # catalog_path = "/Volumes/LaCie/all_andorra/catalog/catalog_obj.pkl"
+    # output_path = "/Volumes/LaCie/all_andorra/catalog/catalog_surf.txt"
+    # wc = WriteCatalog(catalog_path)
+    # wc.write_catalog_surf(output_path)
 
