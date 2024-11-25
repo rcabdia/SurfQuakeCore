e8ab730becd0054d5c4fa37ff8b4800af1c9e25b
cpalalo <117823619+cpalalo@users.noreply.github.com>
2024-11-25 14:02:34 +0100

    Add files via upload

---
+++ tests/test_project.py
@@ -0,0 +1,14 @@
+from multiprocessing import freeze_support
+from surfquakecore.project.surf_project import SurfProject
+
+
+path_to_data = "/home/sysop/Escritorio/geofisica/test_surfquake/inputs/waveforms_cut"
+path_to_project = "/home/sysop/Escritorio/geofisica/test_surfquake/outputs/project/project.pkl"
+
+if __name__ == '__main__':
+
+    freeze_support()
+    sp = SurfProject(path_to_data)
+    sp.search_files()
+    print(sp)
+    sp.save_project(path_file_to_storage=path_to_project)
