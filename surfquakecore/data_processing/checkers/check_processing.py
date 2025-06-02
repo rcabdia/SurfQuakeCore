f14b6c67cbb01f57804b3d04e1da3aa094b338cb
rcabdia <rcabdia@gmail.com>
2025-06-02 12:41:11 +0200

    Correctly working and teted rmean, taper, fiter and add_noise

---
+++ surfquakecore/data_processing/checkers/check_processing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+check_processing
+
+
+
+:param : 
+:type : 
+:return: 
+:rtype: 
+"""
---
+++ surfquakecore/data_processing/processing_methods.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+processing_methods
+
+
+
+:param : 
+:type : 
+:return: 
+:rtype: 
+"""
